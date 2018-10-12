e0a2e20f86d2aa2bf9e6eba5ce8e3c2ecc570283
Gauraang <gauraang.khurana@gmail.com>
2018-10-12 16:40:31 -0400

    HashMap functionality included

--- src/main/java/ncsu/tinyurl/project/Decrypt.java
+++ src/main/java/ncsu/tinyurl/project/Decrypt.java
@@ -1,5 +1,17 @@
 package ncsu.tinyurl.project;
 
+import ncsu.tinyurl.project.UrlMap.AppInstance;
+
 public class Decrypt {
 
+	public AppInstance ins;
+	
+	public Decrypt(AppInstance instance) {
+		this.ins = instance;
+	}
+	
+	public String decryptUrl(String value) {
+		return ins.getUrl(value);
+	}
+	
 }
--- src/main/java/ncsu/tinyurl/project/Encrypt.java
+++ src/main/java/ncsu/tinyurl/project/Encrypt.java
@@ -1,10 +1,20 @@
 package ncsu.tinyurl.project;
 
+import ncsu.tinyurl.project.UrlMap.AppInstance;
+
 public class Encrypt {
 
+	AppInstance ins;
+	
+	public Encrypt(AppInstance instance) {
+		ins = instance;
+	}
+	
 	public String encryptUrl(String url) {
-		
-		String tinyUrl = "asd";
+		//implement a hashMap to return tinyurl for an input here
+		String tinyUrl = "asd";				///Change this to hashmap Url
+
+		ins.addUrl(url, tinyUrl);
 		
 		return tinyUrl;
 	}
