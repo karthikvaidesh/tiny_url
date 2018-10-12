78685cca5cd89e8ff9becd42865d8656a7d344b0
Gauraang <gauraang.khurana@gmail.com>
2018-10-12 15:40:29 -0400

    first commit

---
+++ src/main/java/ncsu/tinyurl/project/App.java
@@ -0,0 +1,13 @@
+package ncsu.tinyurl.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
